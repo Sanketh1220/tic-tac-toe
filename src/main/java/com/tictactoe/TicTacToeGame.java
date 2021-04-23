3f12a5af108be5635b1768b239206243ef7fb82e
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-04-23 21:49:35 +0530

    ADD: board for tic tac toe game

--- src/main/java/com/tictactoe/TicTacToeGame.java
+++ src/main/java/com/tictactoe/TicTacToeGame.java
@@ -1,7 +1,31 @@
-package com.tictactoe;
+/***********************************************
+ * Purpose : Program is written for creating a Tic Tac Toe Game
+ *
+ * @author Sanketh Chigurupalli
+ * @version 1.0
+ * @since 23-04-2021
+ *
+ ***********************************************/
 
+package com.tictactoe;
 public class TicTacToeGame {
+
+    /*
+     * UC - 1
+     * prints the board
+     */
+     static void showBoard() {
+        System.out.println("    |   |    ");
+        System.out.println("*" + "[ ]" + "|" + "[ ]" + "|" + "[ ]" + "*");
+        System.out.println("----|---|----");
+        System.out.println("*" + "[ ]" + "|" + "[ ]" + "|" + "[ ]" + "*");
+        System.out.println("----|---|----");
+        System.out.println("*" + "[ ]" + "|" + "[ ]" + "|" + "[ ]" + "*");
+        System.out.println("    |   |    ");
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
+        showBoard();
     }
 }
